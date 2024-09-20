cdfe487f66aafca8bfe2992de0c8d4a37cd9f655
devid-deshmukh <deviddeshmukh22@gmail.com>
2024-09-20 19:32:53 +0000

    ok

--- one.js
+++ one.js
@@ -1,49 +1,9 @@
-x="i"
-y="am"
 
-z=" devid"
-
-
-console.table([x,y,z])
-let v=undefined
-console.log(typeof v)
-let q="25aids"
-let r=Number(q)
-console.log(typeof q)
-console.log(typeof r)
-const val=Symbol("7")
-const ass=Symbol("7")
-const work=function() {
-    console.log('Hey')
-}
-work()
-let s="Victor"
-console.log(`Hello ${s},I am ${z}`)//string interpolation
-console.log(z.replace(z,s))//replace
-console.log(z.trim())//trim
-const k=12345.4678
-const l=k.toFixed(2)
-console.log(l)
-let m=Math.random()//to print random values of variable every time
-console.log(m)
-let o=new Date()
-console.log(typeof o)
-let fix=o.toLocaleString()
-console.log(fix)
-let t=Date.now()
-console.log(t)
- arr=[2,4,6,8]
-console.log(arr[1])
-console.log(arr)
-arr.reverse()
-console.log(arr)
-const person=
+const ob_j =   //object creation
 {
-    name:"David",
-    sex:"female",
-
-    greet: function() {
-        console.log(`hello world ${this.name} here and am a ${this.sex}`);
-    }
+    name:"Kartik",
+    age:"21"
+    
 }
-person.greet();
+const {name} = ob_j
+console.log(name)//object destruction
